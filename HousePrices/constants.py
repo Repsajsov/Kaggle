dc11176045002b4653e2ef4d7625c05c5e7fc0cf
Repsajsov <jaspervos12@outlook.com>
2025-05-13 19:03:24 +0200

    Starting with house price project.

---
+++ HousePrices/constants.py
@@ -0,0 +1,12 @@
+MSSUBCLASS_INFO = {
+    "1Story": [20, 30, 40, 120],
+    "1.5Story": [45, 50, 150],
+    "2Story": [60, 70, 160],
+    "2.5Story": [75],
+    "SplitLevel": [80, 85, 180],
+    "Post1945": [20, 60, 120, 160],  
+    "Pre1945": [30, 70],
+    "PUD": [120, 150, 160, 180],
+    "Duplex": [90],
+    "TwoFamilyConversion": [190]
+}
---
+++ HousePrices/libraries.py
@@ -0,0 +1,4 @@
+import polars as pl
+
+import matplotlib.pyplot as plt
+import seaborn as sns
